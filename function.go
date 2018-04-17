7bfb8537ae977566e20034695614a91c7e35e4be
MapleLaker <31602441+MapleLaker@users.noreply.github.com>
2018-04-18 06:59:53 +0800

    Update function.go

--- function.go
+++ function.go
@@ -18,20 +18,22 @@ func swap(x, y string) (string, string){
 }
 
 func main() {
+	//单返回值
 	var result int
 	result = max(50,150)
-
 	fmt.Printf("%d", result)
 
-
-
+	//多返回值
 	var name string
 	name = "Monday"
 
 	var author string
 	author = "Sunday"
-// use this pattern to receive the 2 return value at the same time
+	// use this pattern to receive the 2 return value at the same time
 	var result1, result2 string = swap(name, author) 
+	fmt.Printf("%s\n %s\n", result1, result2)
 
-	fmt.Printf("%s\n, %s\n", result1, result2)
+	//简洁
+	a,b:= swap(name, author)
+	fmt.Printf(" a is %s\n b is %s\n", a, b)	 
 }
